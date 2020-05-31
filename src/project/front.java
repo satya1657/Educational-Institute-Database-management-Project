cec3f44d00e0572432ab2739d24eafe187ab9701
satyaprakash <kumarsatyaprakash33@gmail.com>
2020-06-01 00:35:12 +0530

    check for push

--- src/project/front.java
+++ src/project/front.java
@@ -33,7 +33,7 @@ import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
 
 public class front  implements ActionListener{
-    //hello guys how r u?
+    //hello guys how r
 	static JFrame f;
 	private JTextField sid;
 	private JTextField spas;
